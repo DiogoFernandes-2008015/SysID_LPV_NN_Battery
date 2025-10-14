f3aad7f56cbc7f1517f9d9e63bbc72aa22c02286
Diogo_Fernandes <47782824+DiogoFernandes-2008015@users.noreply.github.com>
2025-10-14 10:48:53 -0300

    Update ID_Hyb_Grey_JAX_DNN_1RC.py
    Code update

--- ID_Hyb_Grey_JAX_DNN_1RC.py
+++ ID_Hyb_Grey_JAX_DNN_1RC.py
@@ -1,7 +1,4 @@
-import numpy as np
-import scipy as sc
 from scipy.integrate import odeint
-import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from scipy.io import loadmat
 from tqdm import tqdm # For the progress bar
@@ -14,14 +11,14 @@ import matplotlib.pyplot as plt
 from diffrax import diffeqsolve, Dopri5, ODETerm, SaveAt, LinearInterpolation
 from jax import random, grad, jit, vmap
 from jax.flatten_util import ravel_pytree
-    
+
+#Checking where JAX is running
 try:
     print(f"JAX is running on: {jax.devices()[0].platform.upper()}")
 except IndexError:
     print("No JAX devices found.")
 
 jax.config.update("jax_enable_x64", True)
-
 
 # Loading training dataset
 DATA = loadmat('data_train.mat')
@@ -48,6 +45,12 @@ axs[1].grid(True)
 plt.tight_layout() # Adjusts subplot params for a tight layout
 plt.show()
 
+#Decimation of the signals
+decimate = 5
+u = u[::decimate]
+y = y[::decimate]
+time = time[::decimate]
+
 if time is not None:
     time = time.flatten()
     u = u.flatten()
@@ -57,7 +60,13 @@ if time is not None:
     Ts = time[1] - time[0]
     fs = 1 / Ts
     T = time[-1]
-    print(f"N={N}, fs={fs}, T={T}, Ts={Ts}")
+    print(f"Training Dataset\nN={N}, fs={fs}, T={T}, Ts={Ts}")
+
+# --- Setup for Multiple Shooting ---
+n_shots = 10 # random
+n_timesteps_per_shot = N // n_shots
+n_states = 2
+
 
 # --- Neural Network Helper Functions (from JAX ANN example) ---
 
@@ -106,16 +115,13 @@ def hybrid_battery_1rc_jax(t, x, args):
 
 term = ODETerm(hybrid_battery_1rc_jax)
 
-# --- Setup for Multiple Shooting ---
-n_shots = 100 # random
-n_timesteps_per_shot = N // n_shots
 
 t_shots = jnp.array(time.reshape(n_shots, n_timesteps_per_shot))
 y_data = jnp.array(y.reshape(n_shots, n_timesteps_per_shot))
 u_interpolation = LinearInterpolation(ts=time, ys=u)
 
 # --- NN and Optimization Configuration ---
-nn_layer_sizes = [1, 64, 3]  # 2 inputs (w, u), 2 hidden layers of 10, 1 output
+nn_layer_sizes = [1, 64, 3]  # 1 inputs (SOC), 1 hidden layers of 64, 3 outputs
 solver = Dopri5()
 
 # --- Create Initial Guess and Parameter Structures ---
@@ -125,15 +131,9 @@ initial_params_nn = init_network_params(nn_layer_sizes, key)
 flat_initial_nn_params, params_nn_struct = ravel_pytree(initial_params_nn)
 len_nn_params = len(flat_initial_nn_params)
 
-# 2. Palpite para o PRIMEIRO Estado Inicial (x_0,1)
-# Estado inicial: [SOC, Vc]
+# Initial Guess for the state: [SOC, Vc]
 x_initial_first_shot = jnp.array([0.98, 0.0]) # SOC em 98%, Vc em 0V
-
-# 3. Palpite para os Estados dos Shots Intermediários
 x_initial_shots_repeated = jnp.tile(x_initial_first_shot, n_shots) 
-
-
-# Create the full, flattened initial guess vector for the optimizer
 initial_guess_np = np.concatenate([
     np.array(flat_initial_nn_params),
     x_initial_shots_repeated
@@ -147,7 +147,7 @@ initial_guess_np = np.concatenate([
 def objective_jax_nn(decision_vars):
     # Manually unflatten parameters inside the jitted function
     params_nn = params_nn_struct(decision_vars[:len_nn_params])
-    x_initial_shots = decision_vars[len_nn_params:].reshape(n_shots, 2) 
+    x_initial_shots = decision_vars[len_nn_params:].reshape(n_shots, n_states) 
     #args = (params_nn, u_interp)
 
     def simulate_shot(t_shot, w0):
@@ -159,9 +159,8 @@ def objective_jax_nn(decision_vars):
     # Simulation of the states prediction of each shot
     x_pred = jax.vmap(simulate_shot)(t_shots, x_initial_shots)
     
-    # Função que calcula a saída para UM PASSO de tempo
+
     def model_output_step(t, x_step, u_interp_obj):
-        # Obtemos a corrente (u) usando o objeto interpolador
         u = u_interp_obj.evaluate(t)
         
         p = jnp.array([1.02726610e+03,
@@ -173,27 +172,28 @@ def objective_jax_nn(decision_vars):
                        1.07265101e+03,
                        -1.65017255e+02,
                        1.36600705e+01,
-                       3.10715139e+00]) # Polinômio
+                       3.10715139e+00])
         OCV = jnp.polyval(p, x_step[0])
+        nn_input = jnp.array([x_step[0]])
+        delta_R0, delta_R1, delta_C1 = predict(params_nn, nn_input)
+        R0 = 0.2462*(1+delta_R0)
         # A saída é: OCV + R0*u + Vc (x[1])
-        y_pred_step = OCV + 0.2462 * u + x_step[1]
+        y_pred_step = OCV + R0 * u + x_step[1]
         return y_pred_step
     
     def process_shot_output(t_shot, x_shot, u_interp_obj):
-        # Mapeia sobre o tempo (N_steps). t, x_step variam (0), R0 e u_interp são constantes (None).
         return jax.vmap(model_output_step, in_axes=(0, 0, None))(t_shot, x_shot, u_interp_obj)
     
-    # Mapeia sobre os shots (N_shots). t_shot e x_shot variam (0), R0 e u_interp são constantes (None).
     y_pred = jax.vmap(process_shot_output, in_axes=(0, 0, None))(t_shots, x_pred, u_interpolation)
     
-    
+    #Loss function
     return jnp.sum((y_pred - y_data)**2)
 
 @jit
 def continuity_constraints_jax_nn(decision_vars):
     params_nn_flat = decision_vars[:len_nn_params]
     params_nn = params_nn_struct(params_nn_flat)
-    x_initial_shots = decision_vars[len_nn_params:].reshape(n_shots, 2) 
+    x_initial_shots = decision_vars[len_nn_params:].reshape(n_shots, n_states) 
     args = (params_nn, u_interpolation)
 
     def get_end_state(t_shot, x0):
@@ -248,23 +248,50 @@ print("\nOptimization finished with status:", result.message)
 # --- Extract and Display Results ---
 
 # Unflatten the final optimized parameters
-theta1_est = result.x[0]
-theta3_est = result.x[1]
-params_nn_est = params_nn_struct(result.x[2:len_nn_params+2])
+params_nn_est = params_nn_struct(result.x[:len_nn_params])
 
 # Use the first identified shot state as the initial state for the full simulation
-w0_est = result.x[len_nn_params+2]
+x_initial_estimated = jnp.array(result.x[len_nn_params:len_nn_params+n_states])
+
 
 print("\n--- Identification Results ---")
-print(f"Estimated physical parameters: theta1 = {theta1_est:.4f}, theta3 = {theta3_est:.4f}")
 
 # --- Time-Domain Validation Plot ---
-final_args = (theta1_est, theta3_est, params_nn_est, u_interpolation)
+final_args = (params_nn_est, u_interpolation)
 final_sol = diffeqsolve(term, solver, t0=time[0], t1=time[-1], dt0=Ts, y0=w0_est,
-                        saveat=SaveAt(ts=jnp.array(time)), args=final_args, max_steps=16384)
+                        saveat=SaveAt(ts=jnp.array(time)), args=final_args, max_steps=100000)
 yhat = final_sol.ys.flatten()
-y_hat_train = yhat
-x_train = u
+
+def model_output_step(t, x_step, u_interp_obj):
+    u = u_interp_obj.evaluate(t)
+    
+    p = jnp.array([1.02726610e+03,
+                   -5.13266541e+03,
+                   1.09109051e+04,
+                   -1.28481333e+04,
+                   9.13851696e+03,
+                   -4.01608666e+03,
+                   1.07265101e+03,
+                   -1.65017255e+02,
+                   1.36600705e+01,
+                   3.10715139e+00])
+    OCV = jnp.polyval(p, x_step[0])
+    nn_input = jnp.array([x_step[0]])
+    delta_R0, delta_R1, delta_C1 = predict(params_nn, nn_input)
+    R0 = 0.2462*(1+delta_R0)
+    # A saída é: OCV + R0*u + Vc (x[1])
+    y_pred_step = OCV + R0 * u + x_step[1]
+    return y_pred_step
+
+y_hat = jax.vmap(process_shot_output, in_axes=(0, 0, None))(t_shots,final_sol.ys , u_interpolation)
+
+#Metrics
+MSE = np.mean((y-y_hat)**2)
+y_mean = jnp.mean(y)
+RSS = jnp.sum((y - y_hat)**2)
+TSS = jnp.sum((y - y_mean)**2)
+r2 = 1.0 - (RSS / TSS)
+print(f"R²: {r2:.4f}, MSE = {MSE:.4f}")
 
 plt.figure(figsize=(12, 7))
 plt.plot(time, y, 'k', label='Measured Data (y)', alpha=0.6)
@@ -284,9 +311,10 @@ plt.ylabel('Predicted (yhat)')
 plt.grid(True)
 plt.show()
 
-DATA = loadmat('pythondataval.mat')
-u = DATA['u']
-y = DATA['y']/10
+#Loading validation dataset
+DATA = loadmat('data_val.mat')
+u = DATA['i']
+y = DATA['v']
 time = DATA['t']
 time = time.reshape(-1)
 u = u.reshape(-1)
@@ -299,45 +327,35 @@ Ts = time[1]-time[0]
 fs = 1/Ts
 T = time[-1]  # Total time in seconds
 
-print(N, fs, T, Ts)
+print(f"\nValidation Dataset\nN ={N:.4f}\nfs={fs:.4f}\nT = {T:.4f}\nTs = {Ts:.4f}")
 
-n_shots = 43 # 8150 / 163 = 50 data points per shot.
-n_timesteps_per_shot = N // n_shots
 
-# Reshape data into batches for multiple shooting
-t_shots = jnp.array(time.reshape(n_shots, n_timesteps_per_shot))
-y_data = jnp.array(y.reshape(n_shots, n_timesteps_per_shot))
+# Create a differentiable interpolation object for the input signal
+u_interpolation = LinearInterpolation(ts=time, ys=u)
 
 # Create a differentiable interpolation object for the input signal
 u_interpolation = LinearInterpolation(ts=time, ys=u)
 # Simulate the final model prediction
-# --- Time-Domain Validation Plot ---
-final_args = (theta1_est, theta3_est, params_nn_est, u_interpolation)
-final_sol = diffeqsolve(term, solver, t0=time[0], t1=time[-1], dt0=Ts, y0=w0_est,
-                        saveat=SaveAt(ts=jnp.array(time)), args=final_args, max_steps=16384)
+final_sol = diffeqsolve(term, solver, t0=time[0], t1=time[-1], dt0=Ts, y0=x_initial_estimated, saveat=SaveAt(ts=jnp.array(time)), args=final_args,max_steps=100000)
+# final_sol = diffeqsolve(term, solver, t0=time[0], t1=time[-1], dt0=Ts, y0=y[0], saveat=SaveAt(ts=jnp.array(time)), args=final_args_
 yhat = final_sol.ys.flatten()
+y_hat = jax.vmap(model_output_step, in_axes=(0, 0, None))(time,final_sol.ys, u_interpolation)
 
+# # Plot final results
 plt.figure(figsize=(12, 7))
-plt.plot(time, y, 'k', label='Measured Data (y)', alpha=0.6)
-plt.plot(time, yhat, 'r--', label='Hybrid Model Prediction (y_hat)', linewidth=2)
-plt.plot(time, y - yhat, 'b-', label='Error', linewidth=1)
+plt.plot(time, y, 'k', label='True state', alpha=0.4)
+plt.plot(time, y_hat, 'b--', label='Identified Model Prediction', linewidth=2)
+plt.plot(time, y-y_hat, 'r', label='Residue', linewidth=2)
 plt.xlabel('Time (s)')
-plt.ylabel('Velocity (w)')
-plt.title('Time-Domain Validation of the Hybrid Model')
+plt.title('Model Identification Result')
 plt.legend()
 plt.grid(True)
 plt.show()
 
-plt.figure(figsize=(12, 7))
-plt.plot(y,yhat, 'ko')
-plt.xlabel('Measured (y)')
-plt.ylabel('Predicted (yhat)')
-plt.grid(True)
-plt.show()
-y_hat_val = yhat
-x_valid = u
-
-results = {'x_valid':x_valid,'x_train':x_train,'y_hat_train':y_hat_train,'y_hat_val':y_hat_val}
-
-
-sc.io.savemat('resultados_hib_nn.mat',results)
+#Metrics
+MSE = np.mean((y-y_hat)**2)
+y_mean = jnp.mean(y)
+RSS = jnp.sum((y - y_hat)**2)
+TSS = jnp.sum((y - y_mean)**2)
+r2 = 1.0 - (RSS / TSS)
+print(f"R²: {r2:.4f}, MSE = {MSE:.4f}")
